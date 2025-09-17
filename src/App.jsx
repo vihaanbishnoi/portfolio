20af5f2479b57cd103e541c3f6f4ec2f5fbefe0e
Vihaan <vihaan4263@gmail.com>
2025-09-18 01:24:59 +0530

    Initial portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LiquidEther from '../components/LiquidEther';
+import Navbar from '../components/Navbar';
+
+
+
+const Hero = () => {
+  return (
+    <section id='Hero'>
+    
+    </section>
+  );
+}
+
+export default Hero
