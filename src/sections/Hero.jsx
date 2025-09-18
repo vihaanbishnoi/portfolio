a0e866accb1c9f8b1b79397bbd0ee45f653d7679
Vihaan <vihaan4263@gmail.com>
2025-09-18 15:35:08 +0530

    Day 2

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,19 @@
+/* 
+Home page (Sticky Navbar + Liquid Ether Animation)
+About me, Work experience and Skills
+Projects (Filterable Gallery)
+Contact (Form with validation)
+Footer (Social media links (Icons with links to LinkedIn, GitHub))
+*/
 import React from 'react'
 import LiquidEther from '../components/LiquidEther';
-import Navbar from '../components/Navbar';
-
-
 
 const Hero = () => {
   return (
     <section id='Hero'>
-    
+      <div className="liquid-ether-container relative overflow-hidden w-full h-screen pointer-events-none touch-none">
+        <LiquidEther />
+      </div>
     </section>
   );
 }
